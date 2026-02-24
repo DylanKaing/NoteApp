cd0d01ad30dc467ceaa948dd5c0b30f2f5a7399d
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-02-23 21:55:45 -0500

    plan entries needed

--- backend/src/main/java/com/dylan/backend/entity/Note.java
+++ backend/src/main/java/com/dylan/backend/entity/Note.java
@@ -2,4 +2,12 @@ package com.dylan.backend.entity;
 
 public class Note {
     
+    // Note Name
+    
+    // Date
+    // Icon maybe (unless preview is possible)
+
+    // Note data (blob)
+
+
 }
--- backend/src/main/java/com/dylan/backend/entity/User.java
+++ backend/src/main/java/com/dylan/backend/entity/User.java
@@ -2,4 +2,8 @@ package com.dylan.backend.entity;
 
 public class User {
     
+    // Username
+    // Password
+    // Email
+    
 }
