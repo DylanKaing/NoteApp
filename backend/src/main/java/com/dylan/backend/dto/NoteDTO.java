bd2badb1b85018fd62492aa7bfab702e58ef129f
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-03-24 18:44:21 -0400

    Update NoteDTO

--- backend/src/main/java/com/dylan/backend/dto/NoteDTO.java
+++ backend/src/main/java/com/dylan/backend/dto/NoteDTO.java
@@ -1,6 +1,9 @@
 package com.dylan.backend.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import org.springframework.cglib.core.Local;
 
 public class NoteDTO {
     private Long noteId;
@@ -13,4 +16,68 @@ public class NoteDTO {
     private LocalDateTime updatedAt;
 
     // Getters and setters
+    public Long getNoteId(){
+        return noteId;
+    }
+
+    public void setNoteId(Long noteId){
+        this.noteId = noteId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public String getDrawingData(){
+        return drawingData;
+    }
+
+    public void setDrawingData(String drawingData){
+        this.drawingData = drawingData;
+    }
+
+    public String getPdfPath(){
+        return pdfPath;
+    }
+
+    public void setPdfPath(String pdfPath){
+        this.pdfPath = pdfPath;
+    }
+
+    public String getOwnerUsername(){
+        return ownerUsername;
+    }
+
+    public void setOwnerUsername(String ownerUsername){
+        this.ownerUsername = ownerUsername;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt(){
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt){
+        this.updatedAt = updatedAt;
+    }
+
 }
--- backend/src/main/java/com/dylan/backend/service/NoteService.java
+++ backend/src/main/java/com/dylan/backend/service/NoteService.java
@@ -1,5 +1,15 @@
 package com.dylan.backend.service;
 
+import com.dylan.backend.dto.UserDTO;
+import com.dylan.backend.entity.User;
+import com.dylan.backend.entity.Note;
+import com.dylan.backend.repository.NoteRepository;
+import com.dylan.backend.repository.SharedNoteRepository;
+import com.dylan.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NoteService {
     
 }
