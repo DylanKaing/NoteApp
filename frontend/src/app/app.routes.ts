b480de373dfa7330a48ec23fb4c2caae8cae6172
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-04-08 17:24:08 -0400

    Set the frontend routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { Dashboard } from './dashboard/dashboard';
+import { NoteEditor } from './note-editor/note-editor';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'note-editor/:id', component: NoteEditor },
+];
