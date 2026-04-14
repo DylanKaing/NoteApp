5ef0f356e7ff5479fd359774247a20f2a21b3cfe
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-04-14 18:34:25 -0400

    Update dashboard.ts

--- frontend/src/app/dashboard/dashboard.ts
+++ frontend/src/app/dashboard/dashboard.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit{
 
+  ngOnInit(): void {
+    
+  }
 }
